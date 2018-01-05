b7329116b82274cd3342f8d9d08d4e6329ddfbe8
levi <levi@huaxi100.com>
2018-01-05 13:37:05 +0800

    普通model的存储数据

--- apps/message/views.py
+++ apps/message/views.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from .models import UserMessage
+
 
 def getTest(request):
-
-    return render(request,'messageboard.html')
+    if (request.method == 'POST'):
+        data = {}
+        data['name'] = request.POST.get('name')
+        data['email'] = request.POST.get('email')
+        data['address'] = request.POST.get('address')
+        data['message'] = request.POST.get('message')
+        userMessage = UserMessage()
+        userMessage.name = data['name']
+        userMessage.email = data['email']
+        userMessage.address = data['address']
+        userMessage.message = data['message']
+        userMessage.save()
+        return HttpResponse('success')
+    return render(request, 'messageboard.html')
